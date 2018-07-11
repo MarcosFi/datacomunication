93529a8201829cd1f8e0819e19ecd13ae17d4f79
MarcosFi <MarcosFi@users.noreply.github.com>
2018-07-11 12:24:15 -0300

    Add files via upload

---
+++ parte2-mestre.ino
@@ -0,0 +1,58 @@
+//mestre
+const int DE_RE =  5;  // HIGH:TX, LOW:RX
+void setup() 
+{ 
+  Serial.begin(9600);
+  Serial.setTimeout(100);                //Tempo de espera de 100ms
+  pinMode(DE_RE, OUTPUT);
+  digitalWrite(DE_RE, HIGH); 
+} 
+void loop() 
+{ 
+  if (Serial.available()) {
+    
+   String comando = Serial.readString(); // Lê os comandos
+   if(comando=="S1W"){                   // escrever
+     Serial.print("1");                  // endereço do escravo
+     delay(8);
+     Serial.println("W");                // função escritor
+    }
+     if(comando=="S2W"){                 // escrever
+     Serial.print("2");                  // endereço do escravo
+     delay(8);
+     Serial.println("W");                // função escritor
+    }
+     if(comando=="S1L"){                 // ler
+    Serial.print("1");                   // endereço do escravo
+    delay(8);
+    Serial.print("L ");                  // função leitura
+    Serial.flush();
+    digitalWrite(DE_RE, LOW);            // RS485 como receptor
+    delay(1000);
+    int sensor=Serial.parseInt();
+    Serial.println(sensor);
+   if(Serial.find("i")){
+       Serial.print("OK");
+      delay(1000);
+      //int sensor = Serial.read();
+      int sensor=Serial.parseInt();      // recebe a leitura
+      Serial.println(sensor);            // imprime na tela      
+    }
+    digitalWrite(DE_RE, HIGH);           // RS485 como Transmisor
+    } 
+
+   if(comando=="S2L"){                  // ler
+    Serial.print("2");                  // endereço do escravo
+    delay(8);
+    Serial.print("L ");                 // função leitura
+    Serial.flush();
+    digitalWrite(DE_RE, LOW);           // RS485 como receptor
+   if(Serial.find("i")){
+     delay(10);
+     int sensor=Serial.parseInt();      // recebe a leitura
+     Serial.println(sensor);            // imprime na tela      
+    }
+    digitalWrite(DE_RE, HIGH);          //RS485 como Transmisor
+    } 
+  }
+}
---
+++ parte3.ino
@@ -0,0 +1,109 @@
+#include <ESP8266WiFi.h> // biblioteca para usar as funções de Wifi do módulo ESP8266
+#include "UbidotsESPMQTT.h"
+#define TOKEN "BBFF-tCRqvFa519QyRd1m3mdtDSopwwrZ3l"
+#define MQTTCLIENTNAME "CLIENTEDADOS"
+ 
+
+bool led_state = false;
+ 
+// Definições da rede Wifi
+const char* SSID = "LifeBox";
+const char* PASSWORD = "lifebox123";
+
+Ubidots cliente (TOKEN, MQTTCLIENTNAME);
+
+void callback(char *topic, byte* payload, unsigned int length){
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  if ((char)payload[0]=='1'){
+    digitalWrite(16, HIGH);
+  }
+  else{
+    digitalWrite(16, LOW);
+  }
+Serial.println();
+}
+
+
+ 
+/*
+ * função que conecta o NodeMCU na rede Wifi
+ * SSID e PASSWORD devem ser indicados nas variáveis
+ */
+void reconnectWiFi() 
+{
+  if(WiFi.status() == WL_CONNECTED)
+    return;
+ 
+  WiFi.begin(SSID, PASSWORD);
+ 
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+ 
+  Serial.println();
+  Serial.print("Conectado com sucesso na rede: ");
+  Serial.println(SSID);
+  Serial.print("IP obtido: ");
+  Serial.println(WiFi.localIP());  
+}
+ 
+void initWiFi()
+{
+  delay(10);
+  Serial.print("Conectando-se na rede: ");
+  Serial.println(SSID);
+  Serial.println("Aguarde");
+ 
+  reconnectWiFi();
+}
+ 
+ 
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+  cliente.setDebug(true);
+  
+  Serial.println("nIniciando configuração WiFin");
+  initWiFi();
+ 
+  Serial.println("nConfiguração finalizada, iniciando loopn"); 
+
+  cliente.wifiConnection("LifeBox", "lifebox123");
+  cliente.begin(callback);
+  cliente.ubidotsSubscribe("esp8266", "atuador");
+  }
+ 
+void loop() {
+ 
+  if(!cliente.connected()){
+      cliente.reconnect();
+      cliente.ubidotsSubscribe("Dados_do_node", "atuador");
+      }
+cliente.loop();
+
+for(int j=0; j<5; j++){
+       
+    Serial.print("VAR = "); Serial.println(j);
+   
+    cliente.add("VAR", j);
+  
+    delay(1000);
+    cliente.ubidotsPublish("Dados_do_node");
+    //cliente.setDebug(true);
+
+    //cliente.ubidotsSubscribe("Dados_do_node", "setpoint"));
+    
+    //cliente.ubidotsSubscribe("Dados_do_node", "atuador");
+
+    cliente.loop();
+    
+    
+    delay(100); 
+  } 
+}
